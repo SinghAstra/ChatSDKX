532f69db54f672b381f034b44615856e58b721f6
Astra <abhaypratapsinghwd@gmail.com>
2025-04-10 11:10:49 +0530

    Basic Project Structure

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/interfaces/site";
+
+export const siteConfig: SiteConfig = {
+  name: "ChatSDKX",
+  description: "Template to build Chat App with Vercel SDK.",
+  tagline: "Template to build Chat App with Vercel SDK.",
+  url: "https://chatsdkx.vercel.app",
+  links: {
+    twitter: "https://x.com/XSinghAstra",
+    githubRepo: "https://github.com/SinghAstra/chatsdkx",
+    github: "https://github.com/SinghAstra",
+  },
+};
