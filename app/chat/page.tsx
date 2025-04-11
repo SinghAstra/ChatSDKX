70999c961e595acfacbccadcc82b5a53c0fde30f
Astra <abhaypratapsinghwd@gmail.com>
2025-04-11 11:20:13 +0530

    Added Sidebar Toggle

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
+import { useSidebar } from "@/components/ui/sidebar";
 import { siteConfig } from "@/config/site";
 import React from "react";
+import { SidebarToggle } from "./sidebar-toggle";
 
 const ChatPage = () => {
   const { open, setOpen } = useSidebar();
   return (
     <div className="h-screen flex flex-col">
       <div className="flex items-center gap-2 p-2">
-        <SidebarTrigger />{" "}
+        <SidebarToggle />{" "}
         {!open && (
           <span
             className="text-lg font-medium cursor-pointer"
