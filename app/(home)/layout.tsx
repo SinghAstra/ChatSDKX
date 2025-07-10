c9651fb1fc5629561283a1a4d48ed919f7686222
Astra <abhaypratapsinghwd@gmail.com>
2025-07-10 14:44:19 +0530

    Installed ComponentX

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,8 +1,7 @@
-import Footer from "@/components/home/footer";
-import Navbar from "@/components/home/navbar";
 import { authOptions } from "@/lib/auth-options";
 import { getServerSession } from "next-auth";
-import React, { ReactNode } from "react";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
 
 interface HomeLayoutProps {
   children: ReactNode;
@@ -10,14 +9,11 @@ interface HomeLayoutProps {
 
 const HomeLayout = async ({ children }: HomeLayoutProps) => {
   const session = await getServerSession(authOptions);
-  return (
-    <div className="relative z-0">
-      <div className="absolute top-0 inset-x-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:3rem_3rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] h-[100vh] z-[-1]" />
-      <Navbar user={session?.user} />
-      <main className="mt-12 pt-12 mx-auto w-full ">{children}</main>
-      <Footer />
-    </div>
-  );
+  if (session) {
+    redirect("/chat");
+  }
+
+  return children;
 };
 
 export default HomeLayout;
--- prisma/migrations/20250505150421_default_value_to_title/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Chat" ALTER COLUMN "title" SET DEFAULT 'New Chat';
