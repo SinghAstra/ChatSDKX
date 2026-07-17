f07fa953abba91e509ed29d2c5caba9de6ede2a9
Astra <abhaypratapsinghwd@gmail.com>
2026-07-17 23:09:37 +0530

    Removed Worker

--- apps/worker/src/config/env.ts
+++
@@ -1,5 +0,0 @@
-import { validateWorkerEnv } from "@repo/env";
-import dotenv from "dotenv";
-
-dotenv.config();
-export const env = validateWorkerEnv();
--- apps/worker/src/utils/workspace.ts
+++
@@ -1,8 +0,0 @@
-import os from "node:os";
-import path from "node:path";
-
-export const PRODUCT_NAME = "summary-x";
-
-export function getWorkspacePath(repositoryId: string) {
-  return path.join(os.tmpdir(), PRODUCT_NAME, repositoryId);
-}
