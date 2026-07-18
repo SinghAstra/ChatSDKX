5ccb4d595e2cbb14900a10d80289e21dedc08c86
Astra <abhaypratapsinghwd@gmail.com>
2026-07-18 13:45:51 +0530

    sidebar-chat-item

--- apps/web/features/repo/actions/get-repos-action.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { type ApiResponse, type GetRepositoriesResponse } from "@repo/shared";
-import { repoApi } from "../api/repo-api";
-
-export async function getRepositoriesAction(): Promise<ApiResponse<GetRepositoriesResponse>> {
-  return repoApi.getRepositories();
-}
